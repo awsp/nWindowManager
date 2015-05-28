2373fa2cbeac721015e62be0cfe65ea3ece322a3
awsp <anthonyspwu@gmail.com>
2015-05-28 10:40:33 -1000

    @version 1.0.0-aplha

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
-# nWindowManager
+# nWindowManager - Managing multiple windows for NW.js
 ---------------
-@version Alpha-0.1.0
+@version 1.0.0-alpha
+
+Inspired by: [https://github.com/varunvairavan/node-webkit-window-manager](https://github.com/varunvairavan/node-webkit-window-manager)
+
+NWindowManager manages your pre-defined windows easily by simply calling its unique ID without further configuration. 
+There is no need to manually keep track of which window has already opened nor worry about reopening the same window over again and again. 
+
+
+#### Installation
+Currently only manually installation is availabile sa I haven't publish it to NPM registry. 
+- Manual Installation
+    - Clone this repo
+    - Put it into your app directory
+    - Use `npm link` to create a symlink
+
+#### Usage
+```js
+var gui = require("nw.gui");
+var masterWindow = gui.Window.get();
+var NWindowManager = require('nWindowManager').NWindowManager
+
+global.nWindowManager = new NWindowManager(gui, {
+  foo: {
+    page: "path/to/your/page.html",
+    options: {
+      frame: true,
+      toolbar: false,
+      width: 860,
+      height: 450,
+      show: true,
+      resizable: false
+    }
+  },
+  preferences: {
+    page: "modules/preferences/panel.html"
+  }
+});
+
+onload = function () {
+  masterWindow.show();
+  masterWindow.focus();
+  masterWindow.on("closed", function () {
+    this.hide();
+    // Show close all windows when main window is closed. 
+    global.nWindowManager.closeAll();
+    this.close(true);
+  });
+};
+
+
+// To open a new window
+global.nWindowManager.open("foo");
+
+// To call a function defined in a page of a new window
+global.nWindowManager.get("foo").window.myFunction();
+
+
+```
+
+
+#### Notes
+- Passing data to other windows is still in consideration. 
+
+
+#### Release Notes
+* 2015/05/28 - @version 1.0.0-alpha
+
+
+#### Issues / Error Reporting
+[https://github.com/awsp/nWindowManager/issues](https://github.com/awsp/nWindowManager/issues)
--- boot.js
+++ boot.js
@@ -1,20 +1,36 @@
-var NWindow = (function () {
+/**
+ * NWindow Collection Manager
+ *
+ * @author Anthony S. Wu <anthonyspwu@gmail.com>
+ * @version 1.0
+ */
+
+var NWindow, NWindowManager;
+
+/**
+ * NWindow Class
+ */
+NWindow = (function () {
+  "use strict";
 
   function NWindow(gui, page, options) {
-    if (! page) {
-      console.error("You must provide a valid page.");
-      return false;
-    }
-    if (! gui) {
+    if (!gui) {
       console.error("GUI instance is undefined");
-      return false;
     }
+    if (!page) {
+      console.error("You must provide a valid page.");
+    }
+
     this.gui = gui;
     this.page = page;
     this.options = options || {};
     this.instance = null;
   }
 
+  /**
+   * Spawn a new nWindow using nw.gui API
+   * @returns {null|*}
+   */
   NWindow.prototype.spawn = function () {
     if (this.page) {
       this.instance = this.gui.Window.open(this.page, this.options);
@@ -22,11 +38,17 @@ var NWindow = (function () {
     }
   };
 
+  /**
+   * Destroy the nWindow instance.
+   */
   NWindow.prototype.destroy = function () {
     this.instance = null;
-    return ;
   };
 
+  /**
+   * Check if this nWindow has spawned.
+   * @returns {boolean}
+   */
   NWindow.prototype.hasSpawned = function () {
     return this.instance !== null;
   };
@@ -34,7 +56,11 @@ var NWindow = (function () {
   return NWindow;
 })();
 
-var NWindowManager = (function () {
+/**
+ * NWindowManager Class
+ */
+NWindowManager = (function () {
+  "use strict";
 
   function NWindowManager(gui, nWindowList) {
     this.nWindowList = {};
@@ -47,6 +73,12 @@ var NWindowManager = (function () {
     }
   }
 
+  /**
+   * Initialize a new nWindow.
+   * @param key
+   * @param nWindowData
+   * @returns {NWindowManager}
+   */
   NWindowManager.prototype.add = function (key, nWindowData) {
     if (nWindowData.hasOwnProperty('page')) {
       this.nWindowList[key] = new NWindow(this.gui, nWindowData.page, nWindowData.options);
@@ -54,20 +86,20 @@ var NWindowManager = (function () {
     return this;
   };
 
-  NWindowManager.prototype.exists = function (uid) {
-    return this.nWindowList.hasOwnProperty(uid) === true;
-  };
-
+  /**
+   * Spawn and focus to a new nWindow
+   * @param uid
+   */
   NWindowManager.prototype.open = function (uid) {
-    var self = this;
+    var nWindow = this.find(uid);
 
-    if (this.exists(uid)) {
-      if ( ! this.nWindowList[uid].hasSpawned()) {
-        var instance = this.nWindowList[uid].spawn();
+    if (nWindow) {
+      if (!nWindow.hasSpawned()) {
+        var instance = nWindow.spawn();
         instance.on("loaded", function () {
           this.focus();
           this.on("closed", function () {
-            self.nWindowList[uid].destroy();
+            nWindow.destroy();
             this.close(true);
           });
         });
@@ -75,15 +107,61 @@ var NWindowManager = (function () {
     }
     else {
       // Not in list
+      console.error("Window with uid " + uid + " is not found. ");
     }
   };
 
+  /**
+   * Check if a nWindow with provided uid exists.
+   * @param uid
+   * @returns {boolean}
+   */
+  NWindowManager.prototype.exists = function (uid) {
+    return this.nWindowList.hasOwnProperty(uid) === true;
+  };
+
+  /**
+   * Find a nWindow with its uid
+   * @param uid
+   * @returns {*}
+   */
+  NWindowManager.prototype.find = function (uid) {
+    return this.exists(uid) ? this.nWindowList[uid] : null;
+  };
+
+  /**
+   * Return the instance of nWindow's Window context.
+   * If you would like to look for the nWindow instance, use find() instead.
+   * @param uid
+   * @returns {*}
+   */
+  NWindowManager.prototype.get = function (uid) {
+    return this.find(uid);
+  };
+
+  /**
+   * Close a nWindow by its uid
+   * @param uid
+   */
+  NWindowManager.prototype.close = function (uid) {
+    var nWindow = this.find(uid);
+    if (nWindow) {
+      nWindow.instance.close();
+    }
+  };
+
+  /**
+   * Close all nWindow, useful when quiting an app with multiple windows
+   */
   NWindowManager.prototype.closeAll = function () {
     for (var uid in this.nWindowList) {
-      var win = this.nWindowList[uid];
+      var win = this.find(uid);
       win.instance.close(true);
     }
   };
 
   return NWindowManager;
 })();
+
+exports.NWindowManager = NWindowManager;
+exports.NWindow = NWindow;
